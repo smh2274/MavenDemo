81522d4156638c12b480aef8d4809fb02266b919
smh2274 <1562502339@qq.com>
2017-05-17 21:45:30 +0800

    5.17 update

--- src/main/java/com/shi/chapter2/controller/CustomerServlet.java
+++ src/main/java/com/shi/chapter2/controller/CustomerServlet.java
@@ -1,4 +1,4 @@
-package com.shi.chapter1;
+package com.shi.chapter2.controller;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -6,25 +6,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
  * Created by Bran-Shi on 5/17/2017.
- *
  */
-@WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
+@WebServlet( "/CustomerServlet")
+public class CustomerServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request,response);
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        DateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String currentTime=dateFormat.format(new Date());
-        System.out.println(currentTime);
-        request.setAttribute("currentTime",currentTime);
-        request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request,response);
+
     }
 }
