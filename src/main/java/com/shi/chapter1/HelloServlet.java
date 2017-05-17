ee19131747a63da4cd42f49dc391adb0bd41e4df
smh2274 <1562502339@qq.com>
2017-05-17 17:29:54 +0800

    5.17 update

---
+++ src/main/java/com/shi/chapter1/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.shi.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Bran-Shi on 5/17/2017.
+ *
+ */
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime=dateFormat.format(new Date());
+        System.out.println(currentTime);
+        request.setAttribute("currentTime",currentTime);
+        request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request,response);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Bran-Shi
+  Date: 5/17/2017
+  Time: 2:37 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hello</title>
+</head>
+<body>
+<h1>现在时间：${currentTime}</h1>
+</body>
+</html>
