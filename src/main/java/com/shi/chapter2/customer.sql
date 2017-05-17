ea4b053b5b3ffdc7dcaa7a4755fbf21ac00a74d3
smh2274 <1562502339@qq.com>
2017-05-17 20:41:27 +0800

    5.17 update

---
+++ src/main/java/com/shi/chapter2/customer.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `customer`(
+ `id` INT(20) NOT NULL AUTO_INCREMENT,
+ `name` VARCHAR(255) DEFAULT NULL ,
+ `contact` VARCHAR(255) DEFAULT NULL ,
+ `telephone` VARCHAR(255) DEFAULT NULL ,
+ `email` VARCHAR(255) DEFAULT NULL ,
+ `remark` text,
+ PRIMARY KEY(`id`)
+)ENGINE=InnoDB CHARACTER SET =utf8;
